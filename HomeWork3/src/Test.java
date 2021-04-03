491981352f4b9a21cd83ec6de566c3798fbf812b
sryaa <sryaa@DESKTOP-KQ5V2P2>
2021-04-03 23:47:36 +0300

    First

---
+++ HomeWork3/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Rectangle A=new Area();
+Rectangle P=new Perimeter();
+A.Print();
+A.Calculate(6.5, 10.6);
+P.Print();
+P.Calculate(6.5, 10.6);
+
+	}
+
+}
