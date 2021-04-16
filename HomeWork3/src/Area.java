b64567ccc937c200ac7c8289e6bee195624d3d7e
sryaa <sryaa@DESKTOP-KQ5V2P2>
2021-04-16 14:35:22 +0300

    Second Commit

---
+++ HomeWork3/src/Area.java
@@ -0,0 +1,10 @@
+
+public class Area extends Rectangle {
+
+
+	public void Calculate(double W, double L) {
+		double Area=W*L;
+		System.out.println("Area equal : "+Area);
+	}
+	
+}
---
+++ HomeWork3/src/Rectangle.java
@@ -0,0 +1,12 @@
+// This project for print: 
+// First one to print the area of rectangle 
+// Second to print the perimeter of rectangle 
+
+abstract class Rectangle {
+
+public abstract void Calculate(double W, double L);
+
+public void Print() { // this is normal method with body 
+	System.out.println("After calculated the rectangle, so we have  : ");
+}
+}
