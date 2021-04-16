41e5725ea9916f43fa3fa8aa01e93e748f9beadd
438103166 <71055214+438103166@users.noreply.github.com>
2021-04-16 14:28:13 +0300

    Update Test.java

--- HomeWork3/src/Test.java
+++ HomeWork3/src/Test.java
@@ -3,8 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Rectangle A=new Area();
-Rectangle P=new Perimeter();
+Area A=new Area();
+Perimeter P=new Perimeter();
 A.Print();
 A.Calculate(6.5, 10.6);
 P.Print();
